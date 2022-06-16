61d710cc8e1cea211bf1f71514a7a77dbd8661c2
tramtran0497 <tramtran0497@gmail.com>
2022-06-16 14:58:22 +0300

    Set up and create a new page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Home/>
     </div>
   );
 }
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type HomeProps = {
+
+}
+
+const Home: React.FunctionComponent<HomeProps> = () => (
+    <div>
+        HOME
+    </div>
+);
+
+export default Home;
