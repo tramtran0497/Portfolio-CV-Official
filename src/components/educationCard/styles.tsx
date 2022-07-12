9a360b979099814f4767324d87e3e800ed7db195
tramtran0497 <tramtran0497@gmail.com>
2022-07-12 23:19:12 +0300

    Responsive web design for Education page

--- src/components/educationCard/styles.tsx
+++ src/components/educationCard/styles.tsx
@@ -6,30 +6,74 @@ export const Description = styled.p`
   line-height: 30px;
   letter-spacing: 1px;
   color: white;
-  font-size: 20px;
+  @media only screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+
+  @media only screen and (min-width: 992px) {
+    font-size: 18px;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    font-size: 20px;
+  }
 `;
 export const EducationItem = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   position: relative;
-  margin: 30px;
+  @media only screen and (min-width: 768px) {
+    margin: 15px;
+  }
+
+  @media only screen and (min-width: 992px) {
+    margin: 20px;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    margin: 30px;
+  }
 `;
 
 export const GraduationName = styled.p`
   margin-left: 20px;
-  font-size: 20px;
   cursor: pointer;
 
   &:hover {
     text-decoration: underline;
   }
+  @media only screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+
+  @media only screen and (min-width: 992px) {
+    font-size: 19px;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    font-size: 20px;
+  }
 `;
 
 export const ImageWrapper = styled.div`
-  width: 100px;
-  height: 50px;
-  margin-left: 20px;
+  @media only screen and (min-width: 768px) {
+    width: 80px;
+    height: 40px;
+    margin-left: 10px;
+  }
+
+  @media only screen and (min-width: 992px) {
+    width: 100px;
+    height: 60px;
+    margin-left: 20px;
+  }
+
+  @media only screen and (min-width: 1200px) {
+    width: 100px;
+    height: 60px;
+    margin-left: 30px;
+  }
 `;
 
 export const Image = styled.img`
